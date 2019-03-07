8d7e4d908f46c64ef818ed3d8031caf41f8e3e09
Tolik Dem <tolik.demianov@gmail.com>
2019-03-07 14:06:10 +0300

    сделал input range и на его основе калькулятор цены

---
+++ src/components/inputs/range-slider/range-slider.js
@@ -0,0 +1,51 @@
+class RangeSlider {
+  init(options) {
+    this.containerRange = options.sliderContainer;
+    this.nativeRange = options.sliderContainer.querySelector('input[type="range"]');
+    this.halfTrack = this.containerRange.querySelector('.range-slider-container__half-bg');
+    this.customThumb = this.containerRange.querySelector('.range-slider-container__custom-thumb');
+    this.views = {
+      price: document.querySelector('.pricing-period-money'),
+      days: document.querySelector('.pricing-period-days'),
+    };
+    this.pricePerDay = 6; // цена за день по умолчанию
+
+    this.nativeRange.addEventListener('input', this._inputHandler.bind(this));
+    this._setThumbPosition(50);
+  }
+
+  _inputHandler(e) {
+    const currentValue = parseInt(e.target.value, 10);
+    this._setThumbPosition(currentValue);
+    this._setTrackPosition(currentValue);
+    this._showPrice(currentValue);
+  }
+
+  _setThumbPosition(value) {
+    const containerWidth = parseFloat(this.containerRange.clientWidth);
+    const thumbWidth = parseFloat(this.customThumb.clientWidth);
+    const left = (value / 100) * (containerWidth - thumbWidth);
+    this.customThumb.style.left = `${left}px`;
+  }
+
+  _setTrackPosition(value) {
+    const containerWidth = parseFloat(this.containerRange.clientWidth);
+    const width = (value / 100) * (containerWidth);
+    this.halfTrack.style.width = `${width}px`;
+  }
+
+  setPricePerDay(number) {
+    const price = parseFloat(number);
+    this.pricePerDay = price;
+  }
+
+  _showPrice(days) {
+    const d = days;
+    const totalPrice = d * this.pricePerDay;
+
+    this.views.price.innerHTML = `${totalPrice} / `;
+    this.views.days.innerHTML = ` на ${days} дней `;
+  }
+}
+
+export default RangeSlider;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import './main.scss';
-import card2init from './components/card_2/card_2.js'
-import initCloseModal from './components/modal/modal.js';
+import card2init from './components/card_2/card_2';
+import initCloseModal from './components/modal/modal';
+import RangeSlider from './components/inputs/range-slider/range-slider';
 
-document.addEventListener("DOMContentLoaded", function(event) {
+function main() {
   console.log('test my build');
   card2init();
   initCloseModal();
-});
+  const rangeSlider = new RangeSlider();
+  rangeSlider.init({
+    sliderContainer: document.getElementById('rangeSliderContainerOne'),
+  });
+}
+
+document.addEventListener('DOMContentLoaded', main);
