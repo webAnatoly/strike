a99a9b5149207203d7634ca6400e4c2fc34939e5
Tolik Dem <tolik.demianov@gmail.com>
2019-03-08 15:47:50 +0300

    сделал кликабельной только нижнею часть карточки card_2

--- src/components/card_2/card_2.js
+++ src/components/card_2/card_2.js
@@ -1,6 +1,6 @@
 function card2 () {
   // полчить все карточки с классом card_2
-  const allCards2 = document.querySelectorAll('.card_2');
+  const allCards2 = document.querySelectorAll('.card_2__footer');
 
   // навесить обработчик на каждую карточку
   Array.prototype.forEach.call(allCards2, (card) => {
