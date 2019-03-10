b5f061bbf6a55e01c3a39cb74da950ff2217a244
Tolik Dem <tolik.demianov@gmail.com>
2019-03-10 17:56:45 +0300

    Доработал input-range(калькулятор) в соответствии с пожеланиями клиента
    Клиент хочет, чтобы thumb двигался по вот такой шкале
    1, 7, 8, 9, 10, 11, 12, 13, 14, 16, 18, 20, 22, 24, 26, 28, 30, 45, 60, 90, 180
    каждое значение шкалы это количество дней

--- src/components/inputs/range-slider/range-slider.js
+++ src/components/inputs/range-slider/range-slider.js
@@ -13,15 +13,117 @@ class RangeSlider {
 
     this.nativeRange.addEventListener('input', this._inputHandler.bind(this));
     this._setThumbPosition(50);
+    this._showPrice(30);
   }
 
   _inputHandler(e) {
     const currentValue = parseInt(e.target.value, 10);
-    console.log('currentValue', currentValue);
-    this._setThumbPosition(currentValue);
-    this._setTrackPosition(currentValue);
-    if (currentValue > 0) {
-      this._showPrice(currentValue);
+    // нужна такая вот  шкала 1, 7, 8, 9, 10, 11, 12, 13, 14, 16, 18, 20, 22, 24, 26, 28, 30, 45, 60, 90, 180
+
+    if (currentValue <= 5) { // 1 день
+      this._setThumbPosition(0);
+      this._setTrackPosition(0);
+      this._showPrice(1);
+    }
+    if (currentValue >= 5 && currentValue <= 7) { // 7 дней
+      this._setThumbPosition(5);
+      this._setTrackPosition(5);
+      this._showPrice(7);
+    }
+    if (currentValue >= 8 && currentValue <= 10) { // 8 дней
+      this._setThumbPosition(8);
+      this._setTrackPosition(8);
+      this._showPrice(8);
+    }
+    if (currentValue >= 11 && currentValue <= 13) { // 9 дней
+      this._setThumbPosition(11);
+      this._setTrackPosition(11);
+      this._showPrice(9);
+    }
+    if (currentValue >= 14 && currentValue <= 16) { // 10 дней
+      this._setThumbPosition(14);
+      this._setTrackPosition(14);
+      this._showPrice(10);
+    }
+    if (currentValue >= 17 && currentValue <= 19) { // 11 дней
+      this._setThumbPosition(17);
+      this._setTrackPosition(17);
+      this._showPrice(11);
+    }
+    if (currentValue >= 20 && currentValue <= 22) { // 12 дней
+      this._setThumbPosition(20);
+      this._setTrackPosition(20);
+      this._showPrice(12);
+    }
+    if (currentValue >= 23 && currentValue <= 25) { // 13 дней
+      this._setThumbPosition(23);
+      this._setTrackPosition(23);
+      this._showPrice(13);
+    }
+    if (currentValue >= 26 && currentValue <= 28) { // 14 дней
+      this._setThumbPosition(26);
+      this._setTrackPosition(26);
+      this._showPrice(14);
+    }
+    if (currentValue >= 29 && currentValue <= 31) { // 16 дней
+      this._setThumbPosition(29);
+      this._setTrackPosition(29);
+      this._showPrice(16);
+    }
+    if (currentValue >= 32 && currentValue <= 34) { // 18 дней
+      this._setThumbPosition(32);
+      this._setTrackPosition(32);
+      this._showPrice(18);
+    }
+    if (currentValue >= 35 && currentValue <= 37) { // 20 дней
+      this._setThumbPosition(35);
+      this._setTrackPosition(35);
+      this._showPrice(20);
+    }
+    if (currentValue >= 38 && currentValue <= 40) { // 22 дней
+      this._setThumbPosition(38);
+      this._setTrackPosition(38);
+      this._showPrice(22);
+    }
+    if (currentValue >= 41 && currentValue <= 43) { // 24 дней
+      this._setThumbPosition(41);
+      this._setTrackPosition(41);
+      this._showPrice(24);
+    }
+    if (currentValue >= 44 && currentValue <= 47) { // 26 дней
+      this._setThumbPosition(44);
+      this._setTrackPosition(44);
+      this._showPrice(26);
+    }
+    if (currentValue >= 47 && currentValue <= 50) { // 28 дней
+      this._setThumbPosition(47);
+      this._setTrackPosition(47);
+      this._showPrice(28);
+    }
+    if (currentValue >= 50 && currentValue <= 60) { // 30 дней
+      this._setThumbPosition(50);
+      this._setTrackPosition(50);
+      this._showPrice(30);
+    }
+    if (currentValue >= 60 && currentValue <= 70) { // 45 дней
+      this._setThumbPosition(60);
+      this._setTrackPosition(60);
+      this._showPrice(45);
+    }
+    if (currentValue >= 70 && currentValue <= 80) { // 60 дней
+      this._setThumbPosition(72);
+      this._setTrackPosition(72);
+      this._showPrice(60);
+    }
+    if (currentValue >= 80 && currentValue <= 90) { // 90 дней
+      this._setThumbPosition(85);
+      this._setTrackPosition(85);
+      this._showPrice(90);
+    }
+    if (currentValue > 90) { // 180 дней
+      this._setThumbPosition(100);
+      this._setTrackPosition(100);
+      this._showPrice(180);
     }
   }
 
@@ -43,23 +145,17 @@ class RangeSlider {
     this.pricePerDay = price;
   }
 
-  _showPrice(value) {
-    let day = parseInt(value / 100 * this.maxDays, 10);
+  _showPrice(days) {
+    const d = parseInt(days, 10);
 
-    if (value <= 21) { day = 1; }
-    if (value >= 21 && value <= 45) { day = 14; }
-    if (value >= 45 && value <= 71) { day = 30; }
-    if (value >= 71 && value <= 90) { day = 60; }
-    if (value > 90) { day = 180; }
-
-    const totalPrice = day * this.pricePerDay;
+    const totalPrice = d * this.pricePerDay;
 
     this.views.price.innerHTML = `${totalPrice} / `;
 
-    if (day === 1) {
-      this.views.days.innerHTML = ` на ${day} день `;
+    if (d === 1) {
+      this.views.days.innerHTML = ` на ${d} день `;
     } else {
-      this.views.days.innerHTML = ` на ${day} дней `;
+      this.views.days.innerHTML = ` на ${d} дней `;
     }
   }
 }
